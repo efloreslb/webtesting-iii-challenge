3dfdd440216dd3d52f76482e5cb2480a6675d82f
Edgar F <eflores.ef@gmail.com>
2019-06-26 00:05:36 -0700

    tested display functionality

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+
+   it('renders without crashing', () => {
+      render(<Controls />);
+   })
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,30 @@
-// Test away
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import 'jest-dom/extend-expect';
+
+import Dashboard from './Dashboard';
+
+describe('<Dashboard />', () => {
+
+   it('renders without crashing', () => {
+      render(<Dashboard />);
+   })
+
+   // it('matches snapshot', () => {
+   //    const snap = renderer.create(<Dashboard />);
+   //    expect(snap.toJSON()).toMatchSnapshot();
+   // })
+
+   it('defaults to unlocked and open, options are lock and close', () => {
+      const { getByText } = render(<Dashboard />);
+      const open = getByText(/open/i);
+      const unlocked = getByText(/unlocked/i);
+
+      expect(open).toBeInTheDocument();
+      expect(unlocked).toBeInTheDocument();
+   })
+
+
+})
