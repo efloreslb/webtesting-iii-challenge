e78ead62ee9fb8f3ed1624a568e78b5641b879f1
Edgar F <eflores.ef@gmail.com>
2019-06-26 22:52:52 -0700

    tested controls and dashboard, completed mvp

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 import 'jest-dom/extend-expect';
 
@@ -13,18 +13,35 @@ describe('<Controls />', () => {
    })
 
    it('provides buttons to toggle states', () => {
+      const { getByText } = render(<Controls />);
 
+      const lockbutton = getByText(/lock gate/i);
+      const closebutton = getByText(/close gate/i);
+
+      expect(lockbutton).toBeInTheDocument();
+      expect(closebutton).toBeInTheDocument();
    })
 
    it('should change button text to reflect coming state', () => {
-
+      const { getByTestId } = render(<Controls locked={false} closed={false} />);
+      
+      const toggleclosed = getByTestId('closeButton');
+      expect(toggleclosed).toHaveTextContent('Close Gate');
    })
 
    it('should disable closed toggle button when gate is locked', () => {
+      const { getByTestId } = render(<Controls locked={true} />);
 
+      const closebutton = getByTestId('closeButton');
+
+      expect(closebutton.disabled).toBeTruthy();
    })
 
    it('should disable locked toggle button when gate is open', () => {
+      const { getByTestId } = render(<Controls closed={false} />);
 
+      const lockbutton = getByTestId('lockButton');
+
+      expect(lockbutton.disabled).toBeTruthy();
    })
 })
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/react/cleanup-after-each';
 import 'jest-dom/extend-expect';
 
@@ -12,10 +12,10 @@ describe('<Dashboard />', () => {
       render(<Dashboard />);
    })
 
-   // it('matches snapshot', () => {
-   //    const snap = renderer.create(<Dashboard />);
-   //    expect(snap.toJSON()).toMatchSnapshot();
-   // })
+   it('matches snapshot', () => {
+      const snap = renderer.create(<Dashboard />);
+      expect(snap.toJSON()).toMatchSnapshot();
+   })
 
    it('defaults to unlocked and open, options are lock and close', () => {
       const { getByText } = render(<Dashboard />);
@@ -24,6 +24,26 @@ describe('<Dashboard />', () => {
 
       expect(open).toBeInTheDocument();
       expect(unlocked).toBeInTheDocument();
+   })
+
+   it('buttons and display text changes to reflect the state the door will be in if clicked', () => {
+      const { getByTestId } = render(<Dashboard />);
+      
+      const toggleclosed = getByTestId('closeButton');
+      const displayclosed = getByTestId('closedOpen');
+
+      fireEvent.click(toggleclosed);
+
+      expect(displayclosed).toHaveTextContent('Closed');
+      expect(toggleclosed).toHaveTextContent('Open Gate');
+
+      const togglelocked = getByTestId('lockButton');
+      const displaylocked = getByTestId('lockedUnlocked');
+
+      fireEvent.click(togglelocked);
+
+      expect(displaylocked).toHaveTextContent('Locked');
+      expect(togglelocked).toHaveTextContent('Unlock Gate');
    })
 
 
